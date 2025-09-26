94bac03a1d082c5658c7c3afa7cffbb1cc8e2c2b
RiteshKanwar <riteshkanwar0309@gmail.com>
2025-09-27 04:37:59 +0530

    Fix base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio",
+  base: import.meta.env.MODE === 'production' ? '/portfolio/' : '/',  // Dynamic base: '/' in dev, '/portfolio/' in prod
   resolve: {
     alias: {
         "@": path.resolve(__dirname, "./src")
